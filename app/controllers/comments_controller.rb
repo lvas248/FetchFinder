70bfe59feadf6da9f0fc756c829d0f2feaba7492
Luis <lvas248@gmail.com>
2023-04-19 13:39:16 -0400

    build create controller action for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,15 @@
 class CommentsController < ApplicationController
 
+    def create
+        user = User.find(session[:user_id])
+        comment = user.comments.create(comment_params)
+        render json: comment, status: :created
+    end
+
+    private
+
+    def comment_params
+        params.permit(:park_id, :comment)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_secure_password
 
     has_one :user_image
-    
+    has_many :comments
 
     validates :username, { presence: true, uniqueness: true }
 
