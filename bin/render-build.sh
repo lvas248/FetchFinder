03b5f95792168deaf85050ce8bb28c300e4486ce
Luis <lvas248@gmail.com>
2023-04-19 15:15:42 -0400

    again

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,6 +9,6 @@ cp -a client/build/. public/
 
 # builds the back end code
 bundle install
-bundle exec rake db:drop RAILS_ENV=production
-bundle exec rake db:create db:migrate
+bundle exec rake db:reset RAILS_ENV=production
+bundle exec rake db:migrate
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
