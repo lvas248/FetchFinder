44d2675f5494dbd07486087a9b09a7f4d0e499fa
Luis <lvas248@gmail.com>
2023-05-09 16:05:18 -0400

    create users_at_park_now method in parks serializer

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,6 +1,7 @@
 class Park < ApplicationRecord
     has_many :park_images
     has_many :comments
+    has_many :visits
 
     default_scope {order(:name => :asc)}
 end
--- app/serializers/park_serializer.rb
+++ app/serializers/park_serializer.rb
@@ -1,7 +1,12 @@
 class ParkSerializer < ActiveModel::Serializer
-  attributes :id, :name, :borough, :zip, :surface, :seating, :lat, :long, :address
+  attributes :id, :name, :borough, :zip, :surface, :seating, :lat, :long, :address, :users_at_park_now
   
   has_many :park_images
   has_many :comments
 
+  def users_at_park_now
+    self.object.visits.where('start_time < ?', DateTime.now).where('end_time > ?', DateTime.now).count
+  end 
+
 end
+ 
