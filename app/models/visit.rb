56d24f703950df3974e8cdff682e6abeb548c41d
Luis <lvas248@gmail.com>
2023-05-09 12:29:51 -0400

    update visit model to catch changes to duration or start_time attributes and update end_time

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,5 +1,6 @@
 class Visit < ApplicationRecord
   before_create :set_end_time
+  before_update :update_end_time
 
   belongs_to :user
   belongs_to :park
@@ -10,6 +11,14 @@ class Visit < ApplicationRecord
 
   def set_end_time
     self.end_time = self.start_time + self.duration.to_i
+  end
+
+  def update_end_time
+    # binding.pry
+    if(duration_changed? || start_time_changed? )
+      self.end_time = self.start_time + self.duration.to_i
+    end
+    # binding.pry
   end
   
 end
