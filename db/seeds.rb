0ba3468584050e41b6349f184c7a84a4e0d470d7
Luis <lvas248@gmail.com>
2023-06-16 18:34:27 -0400

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,64 +1,64 @@
-require 'pry'
-require 'geocoder'
-require 'faker'
+# require 'pry'
+# require 'geocoder'
+# require 'faker'
 
 
-response = HTTParty.get("https://data.cityofnewyork.us/resource/hxx3-bwgv.json")
+# response = HTTParty.get("https://data.cityofnewyork.us/resource/hxx3-bwgv.json")
 
-if response.code == 200
-    data = JSON.parse(response.body)  
+# if response.code == 200
+#     data = JSON.parse(response.body)  
     
-    def borough(string)
-        case string
+#     def borough(string)
+#         case string
 
-        when 'M'
-            return 'Manhattan'
-        when 'B'
-            return 'Brooklyn'
-        when 'Q'
-            return 'Queens'
-        when 'X'
-            return 'Bronx'
-        else
-            return 'Staten Island'
-        end
-    end
+#         when 'M'
+#             return 'Manhattan'
+#         when 'B'
+#             return 'Brooklyn'
+#         when 'Q'
+#             return 'Queens'
+#         when 'X'
+#             return 'Bronx'
+#         else
+#             return 'Staten Island'
+#         end
+#     end
 
-    data.map do |p|
-        # binding.pry
-        results = Geocoder.search([p['the_geom']['coordinates'][0][0][0][1], p['the_geom']['coordinates'][0][0][0][0]])
-        puts results
-        Park.create({
-            name: p['name'],
-            borough: borough(p['borough']),
-            zip: p['zipcode'],
-            address: results.first.address
-        }) 
+#     data.map do |p|
+#         # binding.pry
+#         results = Geocoder.search([p['the_geom']['coordinates'][0][0][0][1], p['the_geom']['coordinates'][0][0][0][0]])
+#         puts results
+#         Park.create({
+#             name: p['name'],
+#             borough: borough(p['borough']),
+#             zip: p['zipcode'],
+#             address: results.first.address
+#         }) 
         
     
-    end
+#     end
 
 
   
-    data.each do |p|
-        park = Park.find_by_name(p['name'])
-        park.update( geometry_type: p['the_geom']['type'], coordinates: p['the_geom']['coordinates'][0][0])
-        long = 0
-        lat = 0
-        tot = 0
-        park.coordinates.each do |y,x|
-            long += y
-            lat += x
-            tot +=1
-        end   
+#     data.each do |p|
+#         park = Park.find_by_name(p['name'])
+#         park.update( geometry_type: p['the_geom']['type'], coordinates: p['the_geom']['coordinates'][0][0])
+#         long = 0
+#         lat = 0
+#         tot = 0
+#         park.coordinates.each do |y,x|
+#             long += y
+#             lat += x
+#             tot +=1
+#         end   
 
-        park.update(central_coords: [lat/tot,long/tot])
-        puts "#{lat/tot}, #{long/tot}"
-    end
+#         park.update(central_coords: [lat/tot,long/tot])
+#         puts "#{lat/tot}, #{long/tot}"
+#     end
 
    
     
-end
+# end
 
 
 
